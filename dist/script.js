129726380dd9e9de87fa3ccfc54653ba79209a46
buitantoan <toanbuitan@gmail.com>
2020-08-07 18:06:21 +0700

    section hero and services

--- dist/script.js
+++ dist/script.js
@@ -8,5 +8,11 @@
     /**
      * Script here!
      */
+    $("#btn-search").click(function(e){
+        e.preventDefault();
+        $(".popup").addClass("pop");
+        console.log("hell");
+       
+    });
 
 } )( window )
